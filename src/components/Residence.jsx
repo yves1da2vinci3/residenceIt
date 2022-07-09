944f58a47b78ef0d583cfb33e62e625845447de2
yves1da2vinci3 <diomadelacorano@gmail.com>
2022-07-09 00:26:06 +0000

    correct cards images on homeScreen

--- src/components/Residence.jsx
+++ src/components/Residence.jsx
@@ -4,11 +4,11 @@ import {GiPapers} from 'react-icons/gi'
 
 function Residence({residence}) {
   return (
-<div className='bg-gray-100 w-96 h-[30rem] rounded drop-shadow-lg p-2 cursor-pointer transform hover:-translate-y-4 hover:transition duration-300' >
+<div className='bg-gray-100 w-[26rem] h-[30rem] pb-2 rounded drop-shadow-lg p-2 cursor-pointer transform hover:-translate-y-4 hover:transition duration-300' >
   {/* image et location */}
-<div className='w-full relative'>
+<div className='w-full p-2 relative'>
   {/* image */}
-<img src={residence.imageUrls[0].FileLink} className='h-76  rounded-lg w-76 ' />
+<img src={residence.imageUrls[0].FileLink} className='h-[22rem]  rounded-lg w-full ' />
 {/* location */}
 <div className='w-auto h-12 bg-white p-2 absolute rounded-full flex justify-center items-center top-0'>
 { residence.isAvailable ? <span className="px-2 py-1 text-xs font-bold font-heading border-2 border-blue-300 rounded-full text-blue-300 uppercase bg-white">
